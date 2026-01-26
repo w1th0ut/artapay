583bf5ba175c862f7ccdbd0db2909a5fd55ec4a7
adika <adikanugraha2425@gmail.com>
2026-01-26 10:00:09 +0700

    feat: Add background image to HowItWorksCardItem, migrate to Next/Image, and style marquee items.

--- src/components/HowItWorks/HowItWorksCardItem.tsx
+++ src/components/HowItWorks/HowItWorksCardItem.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import { Marquee } from "@/components/ui/marquee";
+import howItWorksBg from "@/app/assets/how-it-works-bg.png"
 
 interface HowItWorksCardItemProps {
     image: string;
@@ -22,8 +24,9 @@ export default function HowItWorksCardItem({
     return (
         <div className="grid grid-cols-2 w-full h-fit rounded-4xl overflow-hidden border border-white/10">
             {/* Image Section */}
-            <div className={`bg-black-third flex items-center justify-center overflow-hidden ${isReversed ? "order-last" : ""}`}>
-                <img src={image} alt={title} className="w-full h-full object-cover" />
+            <div className={`relative bg-black-third flex items-center justify-center overflow-hidden ${isReversed ? "order-last" : ""}`}>
+                <Image src={howItWorksBg} alt="" className="absolute inset-0 w-full h-full object-cover" />
+                <Image src={image} alt={title} width={500} height={500} className="relative z-10 w-full h-full object-contain p-12" />
             </div>
 
             {/* Content Section */}
@@ -38,7 +41,7 @@ export default function HowItWorksCardItem({
                     <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
                         <Marquee reverse pauseOnHover className="[--duration:20s] text-lg text-white/60">
                             {marqueeItem.map((item, index) => (
-                                <span key={index}>{item}</span>
+                                <span key={index} className="bg-black-third p-2 rounded-md">{item}</span>
                             ))}
                         </Marquee>
                     </div>
